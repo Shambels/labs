5895f211fea6dde641e33230a7e662bd4b71a975
Shambels <MrChnip@gmail.com>
2018-11-05 12:14:05 +0100

    Trying To Do Map

--- resources/views/admin/pages/contact.blade.php
+++ resources/views/admin/pages/contact.blade.php
@@ -10,7 +10,7 @@
 @include('admin.alerts.error')
 @include('admin.pages.partials.pageheader')
 
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2518.693327187696!2d4.3390363157460925!3d50.855362979533275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c38c275028d3%3A0xc7799151146ebf77!2sMolenGeek!5e0!3m2!1sen!2sbe!4v1541413555770" width="100%" height="800" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 
 @include('admin.pages.partials.contact')
